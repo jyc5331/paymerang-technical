1be8ffa769d89cbcda6bb9e0d2b04e3e6f6da6fd
jyc5331 <jcosgrovecoding@gmail.com>
2021-07-29 15:03:20 -0400

    materialize successfully incorporated

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,9 +1,29 @@
 import React from "react";
+import M from "materialize-css"
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems);
+});
 
 const Cards = () => {
     return (
-      <div>cards will go here</div>
+      <ul className="collapsible">
+      <li>
+        <div className="collapsible-header"><i className="material-icons">filter_drama</i>First</div>
+        <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+      </li>
+      <li>
+        <div className="collapsible-header"><i className="material-icons">place</i>Second</div>
+        <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+      </li>
+      <li>
+        <div className="collapsible-header"><i className="material-icons">whatshot</i>Third</div>
+        <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+      </li>
+    </ul>    
     );
   };
+
   
   export default Cards;
