19afb450750622e8f12d779b02b182387fe1dc0e
jyc5331 <jcosgrovecoding@gmail.com>
2021-07-30 11:53:37 -0400

    collapse card done with provided data

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,49 +1,34 @@
 import React from "react";
 import M from "materialize-css"
-// import data from "../utils/paymentData"
+import data from "../utils/paymentData"
 import {Main, Card} from "grommet";
 
-// console.log(data[0].Payee.Name); //don't forget to cap!
+console.log(data[0].Payee.Name); //don't forget to cap!
 
-// for (let i = 0; i < data.length; i++) {
-//   console.log(data[i].Payee.Name)
-// }
-const data = [
-  {
-    project: "Tea App",
-  },
-  {
-    project: "My Wellness Journey",
-  }
-];
-console.log(data)
+for (let i = 0; i < data.length; i++) {
+  console.log(data[i].Payee.Name)
+}
+
 
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.collapsible');
   var instances = M.Collapsible.init(elems);
 });
 
-// const bigData = item => {
-//   item.map(console.log(item))
-// }
-
-// bigData(data);
-
 
 const Cards = () => {
     return (
-      
       <Main>
         <div>
           {data.map((item) => (
-            <Card width="medium" key={item.project}>
-              {item.project}
+            <Card width="medium" key={item.Payee.Name}>
+              {item.Payee.Name}
             </Card>
           ))}
         </div>
       <ul className="collapsible">
       <li>
-        {/* <div className="collapsible-header"><i className="material-icons">filter_drama</i>{data[0].Payee.Name}</div> */}
+        <div className="collapsible-header"><i className="material-icons">filter_drama</i>{data[0].Payee.Name}</div>
         <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
       </li>
       <li>
