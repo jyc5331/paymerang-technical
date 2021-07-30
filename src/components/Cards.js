fb250c675d73a1511563077eb9bf68a636a7cd15
jyc5331 <jcosgrovecoding@gmail.com>
2021-07-30 09:26:13 -0400

    data incorporated, map over data next

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,18 +2,26 @@ import React from "react";
 import M from "materialize-css"
 import data from "../utils/paymentData"
 
-console.log(data);
+console.log(data[0].Payee.Name); //don't forget to cap!
 
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.collapsible');
   var instances = M.Collapsible.init(elems);
 });
 
+// const bigData = item => {
+//   item.map(console.log(item))
+// }
+
+// bigData(data);
+
+
 const Cards = () => {
     return (
+
       <ul className="collapsible">
       <li>
-        <div className="collapsible-header"><i className="material-icons">filter_drama</i>First</div>
+        <div className="collapsible-header"><i className="material-icons">filter_drama</i>{data[0].Payee.Name}</div>
         <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
       </li>
       <li>
@@ -24,7 +32,7 @@ const Cards = () => {
         <div className="collapsible-header"><i className="material-icons">whatshot</i>Third</div>
         <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
       </li>
-    </ul>    
+    </ul>  
     );
   };
 
