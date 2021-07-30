102aff09388c1706c6a970881cfe2a9b0a33ffc7
jyc5331 <jcosgrovecoding@gmail.com>
2021-07-30 11:51:10 -0400

    successful map with simple data

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,13 +1,22 @@
 import React from "react";
 import M from "materialize-css"
-import data from "../utils/paymentData"
-import {Main} from "grommet";
+// import data from "../utils/paymentData"
+import {Main, Card} from "grommet";
 
-console.log(data[0].Payee.Name); //don't forget to cap!
+// console.log(data[0].Payee.Name); //don't forget to cap!
 
-for (let i = 0; i < data.length; i++) {
-  console.log(data[i].Payee.Name)
-}
+// for (let i = 0; i < data.length; i++) {
+//   console.log(data[i].Payee.Name)
+// }
+const data = [
+  {
+    project: "Tea App",
+  },
+  {
+    project: "My Wellness Journey",
+  }
+];
+console.log(data)
 
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.collapsible');
@@ -23,10 +32,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
 const Cards = () => {
     return (
+      
       <Main>
+        <div>
+          {data.map((item) => (
+            <Card width="medium" key={item.project}>
+              {item.project}
+            </Card>
+          ))}
+        </div>
       <ul className="collapsible">
       <li>
-        <div className="collapsible-header"><i className="material-icons">filter_drama</i>{data[0].Payee.Name}</div>
+        {/* <div className="collapsible-header"><i className="material-icons">filter_drama</i>{data[0].Payee.Name}</div> */}
         <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
       </li>
       <li>
